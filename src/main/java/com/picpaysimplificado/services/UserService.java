d0006503d3756703a1d69c188ac273549aa62504
Felipe Oliveira <fbmithrandir@outlook.com>
2024-01-20 00:26:54 -0300

    feat: created UserService

---
+++ src/main/java/com/picpaysimplificado/services/UserService.java
@@ -0,0 +1,33 @@
+package com.picpaysimplificado.services;
+
+import com.picpaysimplificado.domain.user.User;
+import com.picpaysimplificado.domain.user.UserType;
+import com.picpaysimplificado.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public void validateTransaction(User sender, BigDecimal amount) throws Exception {
+        if (sender.getUserType() == UserType.MERCHANT){
+            throw new Exception("Usuários do tipo Lojista não estão autorizados a realizar transações");
+        }
+
+        if(sender.getBalance().compareTo(amount) < 0){
+            throw new Exception("Saldo suficiente");
+        }
+    }
+
+    public User findUserById(Long id) throws Exception {
+        return this.repository.findUserById(id).orElseThrow(() -> new Exception("Usuário não encontrado."));
+    }
+
+    public void saveUser(User user){
+        this.repository.save(user);
+    }
+}
